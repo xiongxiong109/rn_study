d132a95dc59620daa62a06e6925d4d996ca7ab5b
xjq熊剑桥 <jqxiong@Ctrip.com>
2017-01-22 15:54:09 +0800

    style base

--- src/components/app.header.js
+++ src/components/app.header.js
@@ -1,7 +1,9 @@
 // app header
 import React, {Component, PropTypes} from 'react';
 import {
-	Text
+	Text,
+	StyleSheet,
+	View,
 } from 'react-native';
 
 export class AppHeader extends Component {
@@ -10,7 +12,9 @@ export class AppHeader extends Component {
 	}
 	render() {
 		return (
-			<Text>{this.props.title}</Text>
+			<View style={styles.navContainer}>
+				<Text style={styles.navBar}>{this.props.title}</Text>
+			</View>
 		)	
 	}
 }
@@ -18,3 +22,22 @@ export class AppHeader extends Component {
 AppHeader.defaultProps = {
 	title: 'Ctrip'
 }
+
+// 在组件中使用StyleSheet创建组件的样式
+// 在react native中, 并不是所有的样式属性都适合所有的组件, 有的属性只适用于特定的组件
+// rn中的flexDirection的默认值是column, 不是row
+const styles = StyleSheet.create({
+	navContainer: {
+		alignItems: 'center',
+		justifyContent: 'center',
+		flexDirection: 'row',
+		height: 40,
+		backgroundColor: 'rgba(125,125,125,.5)'
+	},
+	navBar: {
+		color: '#c00',
+		flex: 1,
+		textAlign: 'center',
+		backgroundColor: '#dd0'
+	}
+});
