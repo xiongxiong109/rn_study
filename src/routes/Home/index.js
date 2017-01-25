22f6ab6346ee89e8d24b40bbf5ebfc9066ea0d03
xjq熊剑桥 <jqxiong@Ctrip.com>
2017-01-25 11:41:30 +0800

    图片相关

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -3,6 +3,8 @@ import React, {Component} from 'react';
 import {
 	View,
 	Text,
+	Image,
+	StyleSheet,
 	TouchableOpacity
 } from 'react-native';
 import routes from '../index';
@@ -11,10 +13,16 @@ class Home extends Component {
 	render() {
 		return (
 			<View>
-				<Text>Home</Text>
-				<TouchableOpacity onPress={() => {this.goList()}}>
-					<Text>查询</Text>
-				</TouchableOpacity>
+				<View style={styles.bannerContainer}>
+					<Image 
+					source={require('../../imgs/bar.png')}
+					style={styles.banner}
+					>
+						<TouchableOpacity onPress={() => {this.goList()}}>
+							<Text>查询</Text>
+						</TouchableOpacity>
+					</Image>
+				</View>
 			</View>
 		)
 	}
@@ -26,5 +34,16 @@ class Home extends Component {
 		}
 	}
 }
+
+const styles = StyleSheet.create({
+	bannerContainer: {
+		flexDirection: 'row',
+		height: 150
+	},
+	banner: {
+		flex: 1,
+		height: 150
+	}
+});
 
 export default Home
